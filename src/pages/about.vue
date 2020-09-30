b9ddf1c2dd664952b0667d19c54da22ae9d9ec31
clover <750363796@qq.com>
2020-09-30 18:29:11 +0800

    first

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -104,11 +104,11 @@ export default {
     quillEditor
   },
   mounted(){
-    this.$util.addQuillTitle()
+    this.onEditorReady()
   },
   methods: {
     onEditorReady(editor) { // 准备编辑器
- 
+      this.$util.addQuillTitle()
     },
     onEditorBlur(){
       this.$refs.form.validateField('content')
